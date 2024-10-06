c720ab8e412b8e3ece55775f02980171c7d97e03
Bekhal Mohammadi <bekhal.mohammadi@lerniastudent.se>
2024-10-06 21:40:11 +0200

    rest of the files

---
+++ script.js
@@ -0,0 +1,18 @@
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("Det funkar");
+
+function turnIntoVersaler(inputText){ 
+    return inputText.toUpperCase (); 
+
+} 
+
+const inputText = document.getElementById('inputText'); 
+const convertButton = document.getElementById('convertButton'); 
+
+ 
+
+convertButton.addEventListener("click", function() { 
+    inputText.value = turnIntoVersaler(inputText.value);
+
+}); 
+}) 
